8f0d4390ba75ea092c07bfc9c73feb6bf0ebf01f
ShrewdStylegit config --global user.name ShrewdStylegit config --global user.name ShrewdStyle <jake.webtech@gmail.com>
2020-12-01 22:26:23 +0000

    Component re-order and v-for added

--- src/components/Questions/Questions.vue
+++ src/components/Questions/Questions.vue
@@ -1,60 +1,37 @@
 <template>
   <div class="container">
-    <div id="q1" v-if="!q1Active" ref="q1">
-      <h1>{{ questions[0].question }}</h1>
+    <div id="q1" v-if="!isActive">
+      <h1 class="questionTitle">{{ questions[0].question }}</h1>
       <div class="options">
         <button class="optionBtn">{{ questions[0].answers.A }}</button>
         <button class="optionBtn">{{ questions[0].answers.B }}</button>
         <button class="optionBtn">{{ questions[0].answers.C }}</button>
         <button class="optionBtn">{{ questions[0].answers.D }}</button>
-        <button @click="testNext">Test Switch</button>
       </div>
     </div>
-    <div id="q2" v-if="isActive">
-      <Vit-A :questions="questions"></Vit-A>
-    </div>
-    <div id="q3" v-if="isActive">
-      <Vit-B :questions="questions"></Vit-B>
-    </div>
-    <div id="q4" v-if="isActive">
-      <Vit-C :questions="questions"></Vit-C>
-    </div>
-    <div id="q5" v-if="isActive">
-      <Vit-D :questions="questions"></Vit-D>
-    </div>
-    <div id="q6" v-if="isActive">
-      <Vit-E :questions="questions"></Vit-E>
-    </div>
-    <div id="q7" v-if="isActive">
-      <Vit-K :questions="questions"></Vit-K>
-    </div>
-    <div id="q8" v-if="isActive">
-      <Vit-Calc :questions="questions"></Vit-Calc>
+    <div id="q2" v-if="!isActive">
+      <h1 class="questionTitle">{{ questions[1].question }}</h1>
+      <div class="foodContainer" v-for="v in vitAFoods" :key="v.id">
+        <div class="item">
+          <div class="itemName">{{ v.name }}</div>
+          <div class="itemPhoto"></div>
+          <div class="amount">
+            <button class="btnDecrease" @click="foodDecrease(v.id - 1)">
+              -
+            </button>
+            <div class="amountNumber">{{ v.counter }}</div>
+            <button class="btnIncrease" @click="foodIncrease(v.id - 1)">
+              +
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-
-import VitA from './Vitamins/VitA'
-import VitB from './Vitamins/VitB'
-import VitC from './Vitamins/VitC'
-import VitD from './Vitamins/VitD'
-import VitE from './Vitamins/VitE'
-import VitK from './Vitamins/VitK'
-import VitCalc from './Vitamins/VitCalc'
-
 export default {
-
-  components: {
-    VitA,
-    VitB,
-    VitC,
-    VitD,
-    VitE,
-    VitK,
-    VitCalc,
-  },
   data() {
     return {
       results: [],
@@ -72,25 +49,68 @@ export default {
         },
         {
           id: 2,
-          question: "On a weekly basis, select how many times a week you might have the foods shown below for a meal",
+          question:
+            "On a weekly basis, select how many times a week you might have the foods shown below for a meal",
+        },
+      ],
+      vitAFoods: [
+        {
+          id: 1,
+          name: "Eggs",
+          counter: 0,
+        },
+        {
+          id: 2,
+          name: "Fish",
+          counter: 0,
+        },
+        {
+          id: 3,
+          name: "Liver",
+          counter: 0,
+        },
+        {
+          id: 4,
+          name: "Carrots",
+          counter: 0,
+        },
+        {
+          id: 5,
+          name: "Sweet Potatoes",
+          counter: 0,
+        },
+        {
+          id: 6,
+          name: "Red Peppers",
+          counter: 0,
         },
       ],
     };
   },
   methods: {
-    testNext() {
-      this.$refs.q1 = this.isActive = false
-    }
+    foodIncrease(val) {
+      this.vitAFoods[val].counter += 1;
+    },
+    foodDecrease(val) {
+      if (this.vitAFoods[val].counter <= 0) {
+        return 0;
+      } else {
+        this.vitAFoods[val].counter -= 1;
+      }
+    },
   },
 };
 </script>
 
 <style>
-
 .container {
   text-align: center;
   margin-top: 10vh;
   padding: 1rem 4rem;
+}
+
+.questionTitle {
+  margin-bottom: 3rem;
 }
 
 .options {
@@ -109,7 +129,7 @@ export default {
 }
 
 .foodContainer {
-  padding: 4rem;
+  padding: 1rem;
 }
 
 .item {
@@ -126,7 +146,7 @@ export default {
 }
 
 .itemName {
-  padding: 0 .5rem;
+  padding: 0 0.5rem;
   min-width: 200px;
 }
 
@@ -134,8 +154,8 @@ export default {
   padding: 2rem;
 }
 
-.prevQ, .nextQ {
-  margin: 0 .5rem;
+.prevQ,
+.nextQ {
+  margin: 0 0.5rem;
 }
-
 </style>
